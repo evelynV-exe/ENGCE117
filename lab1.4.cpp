80ed208a2b2a1c1a2f986f15f483923e2ec7173d
Evelynnn <93260139+evelynV-exe@users.noreply.github.com>
2025-11-29 19:24:01 +0700

    Enhance GetSet function with input validation
    Added input validation for number of elements and elements themselves.

--- lab1.4.cpp
+++ lab1.4.cpp
@@ -1,29 +1,32 @@
 #include <stdio.h>
 
-int GetSet (int *array[]) {
+int GetSet(int *array[]) {
     static int temp[100];
     int i, n;
 
     printf("Enter number of elements: ");
-    scanf("%d", &n);
+    if (scanf("%d", &n) != 1) n = 0;
+
+    if (n < 0) n = 0;
+    if (n > 100) n = 100;
 
     for (i = 0; i < n; i++) {
         printf("Element %d: ", i + 1);
-        scanf("%d", &temp[i]);
+        if (scanf("%d", &temp[i]) != 1) temp[i] = 0;
     }
 
     *array = temp;
-
     return n;
 }
 
 int main() {
-    int *data, num;
+    int *data, num, i;
     num = GetSet(&data);
 
-    for (int i = 0; i < num; i++) {
+    printf("\nSet elements: ");
+    for (i = 0; i < num; i++) {
         printf("%d ", data[i]);
     }
-    
+
     return 0;
 }
