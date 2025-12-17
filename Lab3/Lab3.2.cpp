d4d0b36af32598d74e8facd86eaa291020e69d21
Evelynnn <93260139+evelynV-exe@users.noreply.github.com>
2025-12-17 08:51:43 +0700

    Update Lab3.2.cpp

---
+++ Lab3/Lab3.2.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct student {
+    char name[20];
+    int age;
+    char sex;
+    float gpa;
+};
+
+void upgrade(struct student *person);
+
+int main() {
+    struct student aboy;
+    aboy.sex = 'F';
+    aboy.gpa = 2.00;
+    upgrade(&aboy);
+    printf("%.2f", aboy.gpa);
+
+    return 0;
+}
+
+void upgrade(struct student *person) {
+
+    if (person->sex == 'M') {
+        person->gpa *= 1.1;   
+    } else if (person->sex == 'F') {
+        person->gpa *= 1.2;
+    }
+}
